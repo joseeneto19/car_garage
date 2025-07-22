55ac5ff403c2c1e23f3c010a5fb3ef808c6c8848
joseeneto19 <josemnetodev@gmail.com>
2025-07-22 14:11:39 -0300

    feat: Create the CarController class.

---
+++ src/main/java/cars/CarController.java
@@ -0,0 +1,10 @@
+package cars;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CarController {
+
+}
