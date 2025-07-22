7ead28eb6c1a13ba469f580af16afbfb700cdbca
joseeneto19 <josemnetodev@gmail.com>
2025-07-22 15:22:49 -0300

    feat: Add Flyway and H2 database dependencies. Create the first database version using Flyway and configure the database URL in application.properties.

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_cars (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    manufacture_year INT NOT NULL,
+    color VARCHAR(30) NOT NULL,
+    license_plate VARCHAR(10) UNIQUE NOT NULL,
+    status VARCHAR(20) DEFAULT 'AVAILABLE' NOT NULL
+);
