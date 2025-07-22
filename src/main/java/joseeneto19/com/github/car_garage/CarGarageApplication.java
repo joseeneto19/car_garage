99dff63d81982565d821a1b4522a3f98176073e7
joseeneto19 <josemnetodev@gmail.com>
2025-07-22 13:24:25 -0300

    first commit

---
+++ src/main/java/joseeneto19/com/github/car_garage/CarGarageApplication.java
@@ -0,0 +1,13 @@
+package joseeneto19.com.github.car_garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGarageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joseeneto19/com/github/car_garage/CarGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package joseeneto19.com.github.car_garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
