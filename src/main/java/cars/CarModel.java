14234fff2c3231da01c33d3a30752e454a67df9b
joseeneto19 <josemnetodev@gmail.com>
2025-07-22 14:31:35 -0300

    refactor: Add Lombok to the CarModel class

--- src/main/java/cars/CarModel.java
+++ src/main/java/cars/CarModel.java
@@ -2,9 +2,15 @@ package cars;
 
 import enums.CarStatus;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cars")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class CarModel {
 
     @Id
@@ -16,64 +22,4 @@ public class CarModel {
     private String color;
     private String licensePlate;
     private CarStatus status;
-
-    public CarModel() {
-    }
-
-    public CarModel(String make, String model, Integer manufactureYear, String color, String licensePlate, CarStatus status) {
-        this.make = make;
-        this.model = model;
-        this.manufactureYear = manufactureYear;
-        this.color = color;
-        this.licensePlate = licensePlate;
-        this.status = status;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public Integer getManufactureYear() {
-        return manufactureYear;
-    }
-
-    public void setManufactureYear(Integer manufactureYear) {
-        this.manufactureYear = manufactureYear;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getLicensePlate() {
-        return licensePlate;
-    }
-
-    public void setLicensePlate(String licensePlate) {
-        this.licensePlate = licensePlate;
-    }
-
-    public CarStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(CarStatus status) {
-        this.status = status;
-    }
 }
